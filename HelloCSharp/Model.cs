45c6309b5f4a4381a03c2974c35d16342e39216a
Your Name <you@example.com>
2018-04-05 19:02:47 +0700

    create function select

--- HelloCSharp/Model.cs
+++ HelloCSharp/Model.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,44 @@ namespace HelloCSharp
 {
     class Model
     {
-        public void Select() {
+        DbConnection connection = new DbConnection();
+        public List<Student> Select() {
+            string query = "SELECT * FROM students";
 
+            //Create a list to store the result
+            List<Student> ListStudent = new List<Student>();
+
+            //Open connection
+            if (connection.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    int id = dataReader.GetInt32("id");
+                    string rollnumber = dataReader.GetString("rollNumber");
+                    string name = dataReader.GetString("name");
+                    Student st = new Student(id, rollnumber, name);
+                    ListStudent.Add(st);
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                connection.CloseConnection();
+
+                //return list to be displayed
+                return ListStudent;
+            }
+            else
+            {
+                return ListStudent;
+            }
         }
 
         public void SelectByRollnumber() {
