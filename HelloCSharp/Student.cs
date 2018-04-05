bc7be9ec510167b51bd5e3c64876c6005ce5fca0
Your Name <you@example.com>
2018-04-05 18:46:13 +0700

    Sua doi Student

--- HelloCSharp/Student.cs
+++ HelloCSharp/Student.cs
@@ -8,5 +8,30 @@ namespace HelloCSharp
 {
     class Student
     {
+        private int id;
+        private string rollNumber;
+        private string name;
+
+        public Student()
+        {
+            
+        }
+
+        public Student(string rollNumber, string name)
+        {
+            this.rollNumber = rollNumber;
+            this.name = name;
+        }
+
+        public Student(int id, string rollNumber, string name)
+        {
+            this.Id = id;
+            this.RollNumber = rollNumber;
+            this.Name = name;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string RollNumber { get => rollNumber; set => rollNumber = value; }
+        public string Name { get => name; set => name = value; }
     }
 }
